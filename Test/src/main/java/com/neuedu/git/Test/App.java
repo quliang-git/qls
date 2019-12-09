c2fc9872ccf9d1523d934e4fa44a4474a47b62a6
quliang <278487775@qq.com>
2019-12-09 14:16:09 +0800

    kke

---
+++ Test/src/main/java/com/neuedu/git/Test/App.java
@@ -0,0 +1,13 @@
+package com.neuedu.git.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
